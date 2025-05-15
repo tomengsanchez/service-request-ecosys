6aa6eee0b9a8d7fb21d4e8e5b7667fb1095dad46
michael sanchez <mrtomy31@gmai.com>
2025-05-15 10:59:04 +0800

    ido

--- includes/views/admin/meta-box-history-view.php
+++ includes/views/admin/meta-box-history-view.php
@@ -1,62 +1,28 @@
 <?php
-// File: includes/helpers/class-srp-notification-helper.php
+// File: includes/views/admin/meta-box-history-view.php
+// Expects $history_log (array of history entries)
 
 if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
-class SRP_Notification_Helper {
+if ( empty( $history_log ) ) {
+    echo '<p>' . __( 'No history recorded yet.', 'service-request-plugin' ) . '</p>';
+} else {
+    echo '<ul style="max-height: 300px; overflow-y: auto; border: 1px solid #eee; padding: 10px;">';
+    // $history_log is already reversed in the model if needed, or reverse here
+    // $history_log = array_reverse($history_log); 
 
-    private $taxonomy_model;
-
-    public function __construct() {
-        $this->taxonomy_model = new SRP_Taxonomy_Model();
-    }
-
-    public function send_admin_notification( $post_id ) {
-        $admin_email = get_option('srp_admin_notification_email', get_option('admin_email'));
-        $subject_meta = get_post_meta($post_id, '_srp_request_subject', true);
-        $title = !empty($subject_meta) ? $subject_meta : get_the_title($post_id);
-
-        $email_subject = sprintf( __( 'New IT Service Request Submitted: #%d - %s', 'service-request-plugin' ), $post_id, $title );
+    foreach ( $history_log as $entry ) {
+        $timestamp = isset($entry['timestamp']) ? date_i18n( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ), $entry['timestamp'] ) : __('Unknown time', 'service-request-plugin');
+        $user_name = isset($entry['user_name']) ? esc_html( $entry['user_name'] ) : __('Unknown user', 'service-request-plugin');
+        $log_text  = isset($entry['entry']) ? esc_html( $entry['entry'] ) : __('No details', 'service-request-plugin');
         
-        $requester_name = get_post_meta( $post_id, '_srp_requester_name', true );
-        $requester_email = get_post_meta( $post_id, '_srp_requester_email', true );
-        $edit_link = admin_url( 'post.php?post=' . $post_id . '&action=edit' );
-
-        $priority = $this->taxonomy_model->get_term_names_for_post($post_id, 'srp_priority_level');
-        $category = $this->taxonomy_model->get_term_names_for_post($post_id, 'srp_service_category');
-        $status   = $this->taxonomy_model->get_term_names_for_post($post_id, 'srp_request_status');
-
-        $message  = sprintf( __( 'A new IT service request "%s" has been submitted.', 'service-request-plugin' ), $title ) . "\n\n";
-        $message .= sprintf( __( 'Request ID: %d', 'service-request-plugin' ), $post_id ) . "\n";
-        $message .= sprintf( __( 'Submitted by: %s (%s)', 'service-request-plugin' ), $requester_name, $requester_email ) . "\n";
-        $message .= sprintf( __( 'Category: %s', 'service-request-plugin' ), $category ) . "\n";
-        $message .= sprintf( __( 'Priority: %s', 'service-request-plugin' ), $priority ) . "\n";
-        $message .= sprintf( __( 'Status: %s', 'service-request-plugin' ), $status ) . "\n";
-        $message .= "\n" . __( 'You can view and manage it here: ', 'service-request-plugin' ) . $edit_link;
-
-        wp_mail( $admin_email, $email_subject, $message );
+        echo '<li style="border-bottom: 1px dotted #ccc; padding-bottom: 5px; margin-bottom: 5px;">';
+        echo '<strong>' . $timestamp . '</strong> - <em>' . $user_name . '</em>:<br />';
+        echo nl2br( $log_text );
+        echo '</li>';
     }
-
-    public function send_user_confirmation( $post_id, $user_email, $user_name ) {
-        $subject_meta = get_post_meta($post_id, '_srp_request_subject', true);
-        $title = !empty($subject_meta) ? $subject_meta : get_the_title($post_id);
-
-        $email_subject = sprintf(__( 'Your IT Service Request (#%d) Has Been Received: %s', 'service-request-plugin' ), $post_id, $title);
-        
-        $priority = $this->taxonomy_model->get_term_names_for_post($post_id, 'srp_priority_level');
-        $category = $this->taxonomy_model->get_term_names_for_post($post_id, 'srp_service_category');
-        $status   = $this->taxonomy_model->get_term_names_for_post($post_id, 'srp_request_status');
-
-        $message  = sprintf( __( 'Dear %s,', 'service-request-plugin' ), $user_name ) . "\n\n";
-        $message .= sprintf( __( 'Thank you for submitting your IT service request: "%s".', 'service-request-plugin' ), $title ) . "\n";
-        $message .= sprintf( __( 'Your Request ID is: %d', 'service-request-plugin' ), $post_id ) . "\n";
-        $message .= sprintf( __( 'Category: %s', 'service-request-plugin' ), $category ) . "\n";
-        $message .= sprintf( __( 'Priority: %s', 'service-request-plugin' ), $priority ) . "\n";
-        $message .= sprintf( __( 'Current Status: %s', 'service-request-plugin' ), $status ) . "\n\n";
-        $message .= __( 'We will process it shortly and get back to you if further information is needed.', 'service-request-plugin' ) . "\n";
-
-        wp_mail( $user_email, $email_subject, $message );
-    }
+    echo '</ul>';
 }
+?>
